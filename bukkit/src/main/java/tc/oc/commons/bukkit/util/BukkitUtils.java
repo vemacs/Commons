ac65c3f4ca600838fdca1950c0764bcb6e0ec54e
vemacs <gentoo@outlook.com>
2013-11-17 09:01:08 -0700

    Use a BiMap for cleanliness

--- bukkit/src/main/java/tc/oc/commons/bukkit/util/BukkitUtils.java
+++ bukkit/src/main/java/tc/oc/commons/bukkit/util/BukkitUtils.java
@@ -80,7 +80,7 @@ public class BukkitUtils {
     }
 
     public static DyeColor chatColorToDyeColor(ChatColor chatColor) {
-        DyeColor dyeColor = CHAT_DYE_MAP.get(chatColor);
+        DyeColor dyeColor = DYE_CHAT_MAP.inverse().get(chatColor);
         if (dyeColor != null) {
             return dyeColor;
         } else {
@@ -88,7 +88,7 @@ public class BukkitUtils {
         }
     }
 
-    private static final Map<DyeColor, ChatColor> DYE_CHAT_MAP = ImmutableMap.<DyeColor, ChatColor>builder()
+    private static final BiMap<DyeColor, ChatColor> DYE_CHAT_MAP = ImmutableBiMap.<DyeColor, ChatColor>builder()
             .put(DyeColor.BLACK, ChatColor.BLACK)
             .put(DyeColor.BLUE, ChatColor.DARK_BLUE)
             .put(DyeColor.GREEN, ChatColor.DARK_GREEN)
@@ -105,24 +105,6 @@ public class BukkitUtils {
             .put(DyeColor.YELLOW, ChatColor.YELLOW)
             .put(DyeColor.WHITE, ChatColor.WHITE)
             .put(DyeColor.PINK, ChatColor.LIGHT_PURPLE).build();
-
-    private static final Map<ChatColor, DyeColor> CHAT_DYE_MAP = ImmutableMap.<ChatColor, DyeColor>builder()
-            .put(ChatColor.AQUA, DyeColor.LIGHT_BLUE)
-            .put(ChatColor.BLACK, DyeColor.BLACK)
-            .put(ChatColor.BLUE, DyeColor.LIGHT_BLUE)
-            .put(ChatColor.DARK_AQUA, DyeColor.CYAN)
-            .put(ChatColor.DARK_BLUE, DyeColor.BLUE)
-            .put(ChatColor.DARK_GRAY, DyeColor.GRAY)
-            .put(ChatColor.DARK_GREEN, DyeColor.GREEN)
-            .put(ChatColor.DARK_PURPLE, DyeColor.PURPLE)
-            .put(ChatColor.DARK_RED, DyeColor.RED)
-            .put(ChatColor.GOLD, DyeColor.ORANGE)
-            .put(ChatColor.GRAY, DyeColor.SILVER)
-            .put(ChatColor.GREEN, DyeColor.LIME)
-            .put(ChatColor.LIGHT_PURPLE, DyeColor.MAGENTA)
-            .put(ChatColor.RED, DyeColor.RED)
-            .put(ChatColor.WHITE, DyeColor.WHITE)
-            .put(ChatColor.YELLOW, DyeColor.YELLOW).build();
 
     public static Color colorOf(ChatColor chatColor) {
         Color color = CHAT_COLOR_MAP.get(chatColor);
